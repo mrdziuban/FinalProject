9596395cd5867ae5e5659a03bf68f584e6366ae1
Matt Dziuban <mrdziuban@gmail.com>
2013-08-05 17:50:17 -0400

    Add players#index view with sorting and pagination. Change backgrounds to say jpg.

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,7 +1,22 @@
 class PlayersController < ApplicationController
   before_filter :authenticate_user!
-  
+  helper_method :sort_column, :sort_direction
+
+  def index
+    @players = Player.order(sort_column + " " + sort_direction).page(params[:page]).per(50)
+  end
+
   def show
     @player = Player.find(params[:id])
+  end
+
+  private
+
+  def sort_column
+    Player.column_names.include?(params[:sort]) ? params[:sort] : "id"
+  end
+
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def sortable(column, title = nil)
+    title ||= column.upcase
+    column = "id" if column == "name"
+    css_class = column == sort_column ? "current #{sort_direction}" : nil
+    puts css_class
+    direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
+    title = "TEAM" if title == "TEAM_ABBREV"
+    title = "+/-" if title == "PLUS_MINUS"
+    title = "S%" if title == "SHOT_PERC"
+    title = "FO%" if title == "FO_PERC"
+    link_to title, {sort: column, direction: direction}, {:class => css_class}
+  end
 end
