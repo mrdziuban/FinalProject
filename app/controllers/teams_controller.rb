1c8e68a59c78cb0a1db0d0eb4eef31e5f7e44df7
Matt Dziuban <mrdziuban@gmail.com>
2013-08-05 16:06:56 -0400

    Add teams#show method.

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -4,4 +4,18 @@ class TeamsController < ApplicationController
   def index
     @standings = Team.standings
   end
+
+  def show
+    @team = Team.find_by_abbrev(params[:abbrev])
+    @standings = Team.standings
+    @standings.each do |div, teams_hash|
+      if teams_hash.keys.include?(@team.abbrev.to_sym)
+        @standings = @standings[div]
+        @standings[:division] = div
+      end
+    end
+    @players = @team.players.sort_by { |p| p.name.split(" ")[1] }
+    @goalies = @team.goalies.sort_by {|g| g.gp}.reverse
+    @games = @team.recent_and_upcoming_games
+  end
 end
