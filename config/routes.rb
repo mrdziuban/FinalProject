8c0f49b70ba2137840ed619a7301895f29da8e53
Matt Dziuban <mrdziuban@gmail.com>
2013-08-07 14:24:20 -0400

    Add routes for goalies.

--- config/routes.rb
+++ config/routes.rb
@@ -9,11 +9,16 @@ GameDay::Application.routes.draw do
     post "fav" => "player_favorites#create"
     delete "fav" => "player_favorites#destroy"
   end
+  resources :goalies, only: :index
+  resources :goalies, only: :show, as: "goalie" do
+    post "fav" => "goalie_favorites#create", as: "fav"
+    delete "fav" => "goalie_favorites#destroy"
+  end
   resources :games, only: [:index, :show]
 
   get "/:abbrev" => "teams#show", as: "team"
   post "/:abbrev/fav" => "team_favorites#create", as: "team_favorites"
-  delete "/:abbrev/fav" => "team_favorites#destroy", as: "team_favorites"
+  delete "/:abbrev/fav" => "team_favorites#destroy"
 
   root to: "home#index"
 end
