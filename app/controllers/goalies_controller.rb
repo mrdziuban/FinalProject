d2486433e4a2296d870b13d86d5f6cb49946ba51
Matt Dziuban <mrdziuban@gmail.com>
2013-08-07 16:20:39 -0400

    Add goalies#index view and AJAX for pagination/sorting.

--- app/controllers/goalies_controller.rb
+++ app/controllers/goalies_controller.rb
@@ -1,9 +1,29 @@
 class GoaliesController < ApplicationController
+  before_filter :authenticate_user!
+  helper_method :sort_column, :sort_direction, :season
+  
   def index
-
+    @goalies = Goalie.order(sort_column + " " + sort_direction).page(params[:page]).per(50)
+    if request.xhr?
+      render partial: "goalies", locals: {goalies: @goalies}
+    end
   end
 
   def show
     @goalie = Goalie.find(params[:id])
+  end
+
+  private
+
+  def sort_column
+    Goalie.column_names.include?(params[:sort]) ? params[:sort] : "id"
+  end
+
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
+  end
+
+  def season
+    nil
   end
 end
