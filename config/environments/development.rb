73ead05ab02266416d21cf9e8da9f993a1e3e08b
Matt Dziuban <mrdziuban@gmail.com>
2013-08-09 10:47:01 -0400

    Add paperclip defaults to production/development to use Amazon S3.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,4 +36,13 @@ GameDay::Application.configure do
   config.assets.debug = true
 
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => "hockeydevelopment",
+      :access_key_id => ENV["S3_ACCESS_KEY_ID"],
+      :secret_access_key => ENV["S3_SECRET_ACCESS_KEY"]
+    }
+  }
 end
