7b51a175eb799d6179fd17262b95fb0ebb03db55
Matt Dziuban <mrdziuban@gmail.com>
2013-08-10 14:23:23 -0400

    Add search bar functionality, results partial, and styling.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,8 +7,14 @@ class HomeController < ApplicationController
 
   def search
     @results = PgSearch.multisearch(params[:query])
+    @teams = @results.select {|r| r.searchable_type == "Team"}
+    @players = @results.select {|r| r.searchable_type == "Player"}
+    @goalies = @results.select {|r| r.searchable_type == "Goalie"}
     if request.xhr?
-      render partial: "results", locals: {results: @results, query: params[:query]}
+      render partial: "results", locals: {teams: @teams, 
+                                          players: @players,
+                                          goalies: @goalies,
+                                          query: params[:query]}
     end
   end
 end
