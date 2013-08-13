1fc33d9dc62bc20b1225d9c000088a645196415c
Matt Dziuban <mrdziuban@gmail.com>
2013-08-13 11:57:02 -0400

    Remove unnecessary pg_search specifications in topic model.

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,9 +1,6 @@
 class Topic < ActiveRecord::Base
   attr_accessible :title, :user_id, :forum_id
 
-  include PgSearch
-  pg_search_scope :search_by_title, against: :title
-
   belongs_to :user
   belongs_to :forum
   validates :title, presence: true
