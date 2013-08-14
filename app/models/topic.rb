6094319a3ed3226da4602ed0b83219ff4e9358cd
Matt Dziuban <mrdziuban@gmail.com>
2013-08-14 09:41:15 -0400

    Add Topic#comments_by_parent.

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -3,6 +3,16 @@ class Topic < ActiveRecord::Base
 
   belongs_to :user
   belongs_to :forum
-  has_many :comments
+  has_many :comments, dependent: :destroy
   validates :title, presence: true
+
+  def comments_by_parent
+    topic_comments = self.comments
+    comments_hash = {}
+    topic_comment.each do |c|
+      comments_hash[c.parent_comment_id] ||= []
+      comments_hash[c.parent_comment_id] << comments
+    end
+    comments_hash
+  end
 end
