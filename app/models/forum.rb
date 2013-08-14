127100999319397aeb0cadd3fc06970830137da5
Matt Dziuban <mrdziuban@gmail.com>
2013-08-14 09:26:09 -0400

    Add dependent destroy relationship between forum and topics.

--- app/models/forum.rb
+++ app/models/forum.rb
@@ -2,5 +2,5 @@ class Forum < ActiveRecord::Base
   attr_accessible :title, :team_abbrev
 
   belongs_to :team, foreign_key: :team_abbrev, primary_key: :abbrev
-  has_many :topics
+  has_many :topics, dependent: :destroy
 end
