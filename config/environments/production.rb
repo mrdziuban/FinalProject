bd302913eab0d956d3c64c5d881f598299fc01d2
Matt Dziuban <mrdziuban@gmail.com>
2013-08-18 15:24:06 -0400

    Add ActionMailer production host.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,9 @@ GameDay::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  config.action_mailer.default_url_options = { :host => 'game-day.heroku.com' }
+
   config.paperclip_defaults = {
     :storage => :s3,
     :s3_credentials => {
